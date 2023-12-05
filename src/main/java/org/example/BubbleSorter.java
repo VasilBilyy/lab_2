16b56516718a4a1e7ce71f82b25033ad660eb723
VasilBilyy <bilyjvasil@gmail.com>
2023-12-05 10:05:31 +0200

    Реалізація bubble sort

--- src/main/java/org/example/BubbleSorter.java
+++ src/main/java/org/example/BubbleSorter.java
@@ -1,11 +1,18 @@
 package org.example;
-
+import java.util.Collections;
 import java.util.ArrayList;
 
 class BubbleSorter implements Sorter {
     public ArrayList<Integer> sort(ArrayList<Integer> input){
         ArrayList<Integer> sortedArray = new ArrayList<>(input);
-
+        int n = sortedArray.size();
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (sortedArray.get(j) > sortedArray.get(j + 1)) {
+                    Collections.swap(sortedArray, j, j + 1);
+                }
+            }
+        }
 
         return sortedArray;
     }
