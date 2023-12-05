41bfb6c392b74bda3e421a7796779d4aad1131b8
VasilBilyy <bilyjvasil@gmail.com>
2023-12-05 10:17:05 +0200

    Реалізація ShellSorter

--- src/main/java/org/example/ShellSorter.java
+++ src/main/java/org/example/ShellSorter.java
@@ -5,7 +5,18 @@ import java.util.ArrayList;
 public class ShellSorter implements Sorter{
     public ArrayList<Integer> sort(ArrayList<Integer> input){
         ArrayList<Integer> sortedArray = new ArrayList<>(input);
+        int n = sortedArray.size();
 
+        for (int gap = n / 2; gap > 0; gap /= 2){
+            for (int i = gap; i < n; i++){
+                int key = sortedArray.get(i);
+                int j = i;
+                while (j >= gap && sortedArray.get(i - gap) > key){
+                    sortedArray.set(j, sortedArray.get(j - gap));
+                    j -= gap;
+                }
+            }
+        }
 
         return sortedArray;
     }
