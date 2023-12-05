5e1a77a4cb3282a140f43ecab24536ddc2407689
VasilBilyy <bilyjvasil@gmail.com>
2023-12-05 23:04:33 +0200

    Реалізація enum SortingType, фабричний метод і всі інші вимоги

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,21 +1,61 @@
 package org.example;
 import java.util.Random;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
 
+enum SortingType {
+    BUBBLE, SHELL, MERGE, QUICK
+}
 
-public class Main {
-    public static void main(String[] args) {
-        Random random = new Random();
-        ArrayList<Integer> array = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            array.add(random.nextInt(10));
+class SortingFactory {
+    public static Sorter getSorter(SortingType type) {
+        switch (type) {
+            case BUBBLE:
+                return new BubbleSorter();
+            case SHELL:
+                return new ShellSorter();
+            case MERGE:
+                return new MergeSorter();
+            case QUICK:
+                return new QuickSorter();
+            default:
+                throw new IllegalArgumentException("Unsupported sorting type: " + type);
         }
-        System.out.println("Original array: " + array);
+    }
+}
+public class Main {
+    private static Random random = new Random();
+    public static void main(String[] args) {
+        for (int count : new int[]{10, 1000, 10000, 1000000}) {
+            System.out.println("Number of elements: " + count);
 
+            ArrayList<Integer> inputArray = generateRandomArray(count);
+            //printArray(inputArray);
 
-        QuickSorter quickSorter = new QuickSorter();
-        ArrayList<Integer> sortedArray = quickSorter.sort(new ArrayList<>(array));
+            for (SortingType type : SortingType.values()) {
+                System.out.println("Sorting type: " + type);
 
-        System.out.println("Sorted array: " + sortedArray);
+                Sorter sorter = SortingFactory.getSorter(type);
+                long startTime = System.currentTimeMillis();
+                ArrayList<Integer> sortedArray = sorter.sort(new ArrayList<>(inputArray));
+                long endTime = System.currentTimeMillis();
+
+                printArray(sortedArray.subList(0, Math.min(50, count)));
+                System.out.println("Time taken: " + (endTime - startTime) + " milliseconds\n");
+            }
+        }
+    }
+
+    private static ArrayList<Integer> generateRandomArray(int count) {
+        ArrayList<Integer> array = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            array.add(random.nextInt(count));
+        }
+        return array;
+    }
+
+    private static void printArray(List<Integer> array) {
+        System.out.println(Arrays.toString(array.toArray(new Integer[0])));
     }
 }
